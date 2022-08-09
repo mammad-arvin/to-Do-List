8003645fc5cb49a2e0338185bc7059c7e0f64159
mohammad hadi arvin <mammad.arvin@gmail.com>
2022-08-09 21:20:11 +0430

    every reload & todo filter done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,27 @@ const inputContainer=document.querySelector(".input");
 const input=inputContainer.firstElementChild;
 const submit=inputContainer.lastElementChild;
 
+                    // every time load 
+document.addEventListener('DOMContentLoaded',getSavedTodo);
+function getSavedTodo(){
+    validation();
+    allWork.forEach(todo => {
+        const lists=document.querySelector('.lists');
+        const listShow=document.createElement('div')
+        listShow.className='listShow';
+        const listShowP=document.createElement('p');
+        listShowP.className='listShowPara';
+        const listShowICheck=document.createElement('i');
+        listShowICheck.className='checkBox fa-solid fa-square-check iconStyle';
+        const listShowITrash=document.createElement('i');
+        listShowITrash.className='deleteBox fa-solid fa-trash-can iconStyle';
+        listShow.appendChild(listShowP);
+        listShow.appendChild(listShowICheck);
+        listShow.appendChild(listShowITrash);
+        lists.appendChild(listShow);
+        listShow.firstChild.innerText=todo;
+    });
+}
 
                     // add listShow
 function addingListShow(){
@@ -36,7 +57,6 @@ function submitToLocal(){
     if(input.value){
         validation();
         allWork.push(input.value);
-        console.log(allWork);
         localStorage.setItem('allWork',JSON.stringify(allWork))
         addingListShow();
     }
@@ -51,13 +71,12 @@ input.addEventListener('keydown',enterKey)
 
 
 
-                        // handel completed and uncompleted
+                  // handel completed and uncompleted button and remove from localStorage
 
 document.querySelector('.lists').addEventListener('click',completedAndUncompleted);
 
 function completedAndUncompleted(event){
     const item=event.target;
-    // console.log(item);
     if(item.classList[0]=== 'checkBox'){
         const previousEl=item.previousElementSibling;
         previousEl.classList.toggle('checked');
@@ -73,4 +92,39 @@ function removeFromLocal(item){
     const todoText=item.firstElementChild.innerText;
     allWork.splice(allWork.indexOf(todoText),1);
     localStorage.setItem('allWork',JSON.stringify(allWork));
+}
+
+                                // filter todos
+const lists=document.querySelector('.lists');
+const todoFiler=document.querySelector('#todoFiler');
+todoFiler.addEventListener('click',filterTodos);
+
+function filterTodos(event){   
+
+    const todos=lists.children;
+
+    for(let child of todos){
+        const listShowPara=child.firstChild;
+        switch(event.target.value){
+            case "all":
+                child.style.display="flex";
+                break;
+            case "completed":
+                if(listShowPara.classList.contains('checked')){
+                    child.style.display="flex";
+                    break
+                }else{
+                    child.style.display="none";
+                }
+                break;
+            case "uncompleted":
+                if(listShowPara.classList.contains('checked')){
+                    child.style.display="none";
+                    break
+                }else{
+                    child.style.display="flex";
+                }
+                break;
+        }
+    }
 }
